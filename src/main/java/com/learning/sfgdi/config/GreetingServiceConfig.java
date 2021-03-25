b1ae1af99fafc7bd4ead1632d0f2bac2573a7662
231332 <anuj.jain@telus.com>
2021-03-25 14:36:54 +0530

    Java Configuration with Primary and Profiles

--- src/main/java/com/learning/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/learning/sfgdi/config/GreetingServiceConfig.java
@@ -2,9 +2,14 @@ package com.learning.sfgdi.config;
 
 import com.learning.sfgdi.controllers.PropertyInjectedController;
 import com.learning.sfgdi.services.ConstructorGreetingServiceImpl;
+import com.learning.sfgdi.services.I18nEnglishGreetingService;
+import com.learning.sfgdi.services.PrimaryGreetingService;
 import com.learning.sfgdi.services.PropertyInjectedGreetingService;
+import com.learning.spanish.I18nSpanishService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -17,5 +22,23 @@ public class GreetingServiceConfig {
     @Bean
     PropertyInjectedGreetingService propertyInjectedGreetingService(){
         return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    @Primary
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    @Profile("EN")
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Bean("i18nService")
+    @Profile({"ES", "default"})
+    I18nSpanishService i18nSpanishService(){
+        return new I18nSpanishService();
     }
 }
--- src/main/java/com/learning/spanish/I18nSpanishService.java
+++ src/main/java/com/learning/spanish/I18nSpanishService.java
@@ -1,11 +1,7 @@
 package com.learning.spanish;
 
 import com.learning.sfgdi.services.GreetingService;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Profile({"ES", "default"})
-@Service("i18nService")
 public class I18nSpanishService implements GreetingService {
     @Override
     public String sayGreeting() {
