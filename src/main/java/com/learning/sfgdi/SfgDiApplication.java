960811ff5a0060012de860758c9fba318028b3e9
231332 <anuj.jain@telus.com>
2021-03-25 15:37:09 +0530

    Bean Scopes Singleton and Prototype

--- src/main/java/com/learning/sfgdi/SfgDiApplication.java
+++ src/main/java/com/learning/sfgdi/SfgDiApplication.java
@@ -5,6 +5,8 @@ import com.learning.sfgdi.controllers.I18nController;
 import com.learning.sfgdi.controllers.MyController;
 import com.learning.sfgdi.controllers.PropertyInjectedController;
 import com.learning.sfgdi.controllers.SetterInjectedController;
+import com.learning.sfgdi.services.PrototypeBean;
+import com.learning.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -35,6 +37,17 @@ public class SfgDiApplication {
 		System.out.println("------------ Constructor");
 		final ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("--------------Bean Scopes ------------------");
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
 	}
 
 }
